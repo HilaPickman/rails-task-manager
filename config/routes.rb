5c9ca26a2da01d9324ac451060463304326f5906
Hila Pickman <pickman.hila@gmail.com>
2022-07-02 16:06:49 +0200

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   #read
-  get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
-  #creat
+  get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: "task"
+  get 'tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
   post 'tasks', to: 'tasks#create'
 
-  #update
-  get 'tasks:/id/edit', to: 'tasks#edit'
+
   patch 'tasks/:id', to: 'tasks#update'
 
-  #delete
+
   delete 'tasks/:id', to: 'tasks#destroy'
 end
